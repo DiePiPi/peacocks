ba67a44640dcfed0a96dd12f594b32a64704bea3
DiePiPi <carybest2002@gmail.com>
2017-10-16 23:52:35 -0700

    add all

---
+++ laundry/views.py
@@ -0,0 +1,15 @@
+#laundry
+
+from django.shortcuts import render
+
+from django.http import HttpResponse,HttpResponseRedirect
+
+
+
+# Create your views here.
+
+def nothing_here(request):
+	return render(request, '404.html')
+	
+def home(request):
+	return render(request, 'home.html', {'hello': 'Welcone to Peacocks!','title':'Peacocks'})
